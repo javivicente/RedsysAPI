90e649d1995154a4620888bf2adf4fa8f3f2ea68
Javi Vicente <javier.vicente.r@gmail.com>
2015-11-09 11:46:55 +0100

    Update petition_example.py

--- petition_example.py
+++ petition_example.py
@@ -4,7 +4,7 @@ from redsysAPI import Client
 REDSYS_MERCHANT_CODE = 'your_merchant_code_here'
 REDSYS_SECRET_KEY = 'your_secret_key_here'
 
-# State if you are using the test enviroment (True)
+# State if you are using the test environment (True)
 # or the real environment for payments (False)
 SANDBOX = True
 
@@ -12,11 +12,11 @@ values = {
             'Ds_Merchant_MerchantCode': REDSYS_MERCHANT_CODE,
             'Ds_Merchant_Terminal': 'your_terminal',
             'Ds_Merchant_TransactionType': '0',
-            'Ds_Merchant_Amount': 5.5, # the module is prepare to transform floats to the format required by REDSYS
+            'Ds_Merchant_Amount': 5.5, # the module is prepared to transform floats to the format required by REDSYS
             'Ds_Merchant_Currency': '978', #Euros
             'Ds_Merchant_Order': 'your_unique_order_number',
             'Ds_Merchant_MerchantURL': 'your_merchant_URL',
-            'Ds_Merchant_ProductDescription': _(u'Service Fee'),
+            'Ds_Merchant_ProductDescription': 'Service Fee',
             'Ds_Merchant_Titular': 'your customer name',
             'Ds_Merchant_UrlOK': 'OK url',
             'Ds_Merchant_UrlKO': 'KO url',
@@ -28,6 +28,6 @@ values = {
 # We initiate the API with our merchant code and secret key
 redsyspayment = Client(business_code=REDSYS_MERCHANT_CODE, priv_key=REDSYS_SECRET_KEY, sandbox=SANDBOX)
         
-# obtain payment button and the merchant order from the values supplied:
+# We obtain the payment button and the merchant order from the values supplied:
 form, merchant_order = redsyspayment.generate_petition(values)
 
